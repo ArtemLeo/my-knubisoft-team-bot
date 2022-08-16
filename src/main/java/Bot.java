ec02c06630504274b51674673caad21e20063f0a
ArtemLeo <link.by.artem@gmail.com>
2022-08-16 22:30:51 +0300

    Update files

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,11 +6,9 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class Bot extends TelegramLongPollingBot {
+
     final private String botToken = "";
-    final private String botName = "@MyKnubiSoftTeamBot";
-
-
-
+    final private String botName = "";
 
     @Override
     public String getBotUsername() {
@@ -48,11 +46,9 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public String parseMessage(String textMsg) {
-        String response = null;
-
-        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
-        if (textMsg.equals("/start"))
-            response = "Привет\uD83D\uDD90\n" +
+        String response;
+        response = switch (textMsg) {
+            case "/start" -> "Привет\uD83D\uDD90\n" +
                     "\n" +
                     "\"КОМАНДА\" \n" +
                     "\"РАЗВИТИЕ\"\n" +
@@ -64,64 +60,58 @@ public class Bot extends TelegramLongPollingBot {
                     "\n" +
                     "Просто сделай выбор\uD83D\uDC47:\n" +
                     "\n" +
-                    "/whoWeAre? \uD83E\uDDD0\n" +
+                    "\uD83E\uDD14 /whoWeAre?\n" +
                     "\n" +
-                    "/whereWeAre? \uD83D\uDCE1\n" +
+                    "\uD83D\uDCE1 /whereWeAre?\n" +
                     "\n" +
-                    "/howWeTeachAndLearn? \uD83D\uDC68\u200D\uD83C\uDF93\n" +
+                    "\uD83D\uDC68\u200D\uD83C\uDF93 /howWeTeachAndLearn?\n" +
                     "\n" +
-                    "/howWeHaveFun? \uD83E\uDD73\n" +
+                    "\uD83E\uDD73 /howWeHaveFun?\n" +
                     "\n" +
-                    "/howWeAreHelping? \uD83E\uDD1D";
-
-        else if (textMsg.equals("/whoWeAre"))
-            response = "Knubisoft - это IT компания с большим опытом в области финансовых технологий, логистики, здравоохранения и банковского дела, которая разрабатывает собственные продукты, а также реализует проекты под ключ для клиентов по всему миру.\n" +
-                    "\n" +
-                    "\uD83E\uDDD0 \uD83D\uDCB0 \uD83E\uDE7A \uD83D\uDCB6 \uD83D\uDD10 \uD83C\uDF0E\n" +
-                    "\n" +
-                    "Cool, right?) Вот следующий пункт\uD83D\uDC47:\n" +
-                    "\n" +
-                    "/whereWeAre? \uD83D\uDCE1\n" +
-                    "\n" +
-                    "/howWeTeachAndLearn? \uD83D\uDC68\u200D\uD83C\uDF93\n" +
-                    "\n" +
-                    "/howWeHaveFun? \uD83E\uDD73\n" +
-                    "\n" +
-                    "/howWeAreHelping? \uD83E\uDD1D";
-
-        else if (textMsg.equals("/whereWeAre"))
-            response = "У команды Knubisoft два \"зеленых\" уютных офиса в самом сердце Харькова.\n" +
+                    "\uD83E\uDD1D /howWeAreHelping?";
+            case "/whoWeAre" ->
+                    "Knubisoft - это IT компания с большим опытом в области финансовых технологий, логистики, здравоохранения и банковского дела, которая разрабатывает собственные продукты, а также реализует проекты под ключ для клиентов по всему миру.\n" +
+                            "\n" +
+                            "\uD83E\uDDD0 \uD83D\uDCB0 \uD83E\uDE7A \uD83D\uDCB6 \uD83D\uDD10 \uD83C\uDF0E\n" +
+                            "\n" +
+                            "Cool, right?) Вот следующий пункт\uD83D\uDC47:\n" +
+                            "\n" +
+                            "\uD83D\uDCE1 /whereWeAre?\n" +
+                            "\n" +
+                            "\uD83D\uDC68\u200D\uD83C\uDF93 /howWeTeachAndLearn?\n" +
+                            "\n" +
+                            "\uD83E\uDD73 /howWeHaveFun?\n" +
+                            "\n" +
+                            "\uD83E\uDD1D /howWeAreHelping?";
+            case "/whereWeAre" -> "У команды Knubisoft два \"зеленых\" уютных офиса в самом сердце Харькова.\n" +
                     "Также мы работаем удаленно по всей Украине и поддерживаем идею найма талантливых людей из разных городов и стран мира.\n" +
                     "\n" +
                     "\uD83C\uDF3F \uD83C\uDFE3 \uD83E\uDDE1 \uD83C\uDF07 \uD83E\uDD1D \uD83D\uDC69\u200D\uD83C\uDF93 \uD83C\uDF0E\n" +
                     "\n" +
                     "Ты молодец) Click more\uD83D\uDC47:\n" +
                     "\n" +
-                    "/howWeTeachAndLearn? \uD83D\uDC68\u200D\uD83C\uDF93\n" +
+                    "\uD83D\uDC68\u200D\uD83C\uDF93 /howWeTeachAndLearn?\n" +
                     "\n" +
-                    "/howWeHaveFun? \uD83E\uDD73\n" +
+                    "\uD83E\uDD73 /howWeHaveFun?\n" +
                     "\n" +
-                    "/howWeAreHelping? \uD83E\uDD1D\n" +
+                    "\uD83E\uDD1D /howWeAreHelping?\n" +
                     "\n" +
-                    "/whoWeAre? \uD83E\uDDD0";
-
-        else if (textMsg.equals("/howWeTeachAndLearn"))
-            response = "Мы стремимся создать сильное сообщество высококвалифицированных специалистов, поэтому особенно гордимся нашим \"Education Center\", в котором команда Knubisoft помогает молодым специалистам получить знания и первую работу.\n" +
-                    "\n" +
-                    "\uD83D\uDC68\u200D\uD83C\uDFEB \uD83D\uDC68\u200D\uD83D\uDCBB \uD83D\uDE24 \uD83D\uDC68\u200D\uD83D\uDCBB \uD83D\uDE2B \uD83D\uDC68\u200D\uD83D\uDCBB \uD83D\uDC69\u200D\uD83C\uDF93 \uD83E\uDD1D \uD83C\uDFE3\n" +
-                    "\n" +
-                    "Not tired?) Тогда продолжим\uD83D\uDC47:\n" +
-                    "\n" +
-                    "/howWeHaveFun? \uD83E\uDD73\n" +
-                    "\n" +
-                    "/howWeAreHelping? \uD83E\uDD1D\n" +
-                    "\n" +
-                    "/whoWeAre? \uD83E\uDDD0\n" +
-                    "\n" +
-                    "/whereWeAre? \uD83D\uDCE1";
-
-        else if (textMsg.equals("/howWeHaveFun"))
-            response = "Мы поддерживаем традицию Knubisoft: \n" +
+                    "\uD83E\uDD14 /whoWeAre?";
+            case "/howWeTeachAndLearn" ->
+                    "Мы стремимся создать сильное сообщество высококвалифицированных специалистов, поэтому особенно гордимся нашим \"Education Center\", в котором команда Knubisoft помогает молодым специалистам получить знания и первую работу.\n" +
+                            "\n" +
+                            "\uD83D\uDC68\u200D\uD83C\uDFEB \uD83D\uDC68\u200D\uD83D\uDCBB \uD83D\uDE24 \uD83D\uDC68\u200D\uD83D\uDCBB \uD83D\uDE2B \uD83D\uDC68\u200D\uD83D\uDCBB \uD83D\uDC69\u200D\uD83C\uDF93 \uD83E\uDD1D \uD83C\uDFE3\n" +
+                            "\n" +
+                            "Not tired?) Тогда продолжим\uD83D\uDC47:\n" +
+                            "\n" +
+                            "\uD83E\uDD73 /howWeHaveFun?\n" +
+                            "\n" +
+                            "\uD83E\uDD1D /howWeAreHelping?\n" +
+                            "\n" +
+                            "\uD83E\uDD14 /whoWeAre?\n" +
+                            "\n" +
+                            "\uD83D\uDCE1 /whereWeAre?";
+            case "/howWeHaveFun" -> "Мы поддерживаем традицию Knubisoft: \n" +
                     "\n" +
                     "\"Раздели веселье с коллегами в конце рабочей недели!)\"\n" +
                     "\n" +
@@ -131,16 +121,14 @@ public class Bot extends TelegramLongPollingBot {
                     "\n" +
                     "Осталось немного!) Click on\uD83D\uDC47:\n" +
                     "\n" +
-                    "/howWeAreHelping? \uD83E\uDD1D\n" +
+                    "\uD83E\uDD1D /howWeAreHelping?\n" +
                     "\n" +
-                    "/whoWeAre? \uD83E\uDDD0\n" +
+                    "\uD83E\uDD14 /whoWeAre?\n" +
                     "\n" +
-                    "/whereWeAre? \uD83D\uDCE1\n" +
+                    "\uD83D\uDCE1 /whereWeAre?\n" +
                     "\n" +
-                    "/howWeTeachAndLearn? \uD83D\uDC68\u200D\uD83C\uDF93";
-
-        else if (textMsg.equals("/howWeAreHelping"))
-            response = "Для каждого из команды Knubisoft важно быть волонтером.\n" +
+                    "\uD83D\uDC68\u200D\uD83C\uDF93 /howWeTeachAndLearn?";
+            case "/howWeAreHelping" -> "Для каждого из команды Knubisoft важно быть волонтером.\n" +
                     "Благодаря Вам, мы уже отправили десятки тонн гуманитарной помощи и сейчас продолжаем поставлять необходимое оборудование для наших парней на передовую линию обороны: рации, дроны, наушники для артиллерии.\n" +
                     "\n" +
                     "Take care!\n" +
@@ -148,26 +136,30 @@ public class Bot extends TelegramLongPollingBot {
                     "\n" +
                     "\uD83D\uDC68 \uD83E\uDDD1 \uD83C\uDD98 \uD83D\uDCB3 \uD83E\uDD1D \uD83D\uDE9A \uD83C\uDF71 \uD83D\uDE4C\n" +
                     "\n" +
+                    "Catch our links\uD83D\uDC47:\n" +
+                    "\n" +
+                    "\uD83D\uDC68\u200D\uD83D\uDCBB knubisoft.com\n" +
+                    "\n" +
+                    "\uD83D\uDC69\u200D\uD83D\uDCBB linkedin.com/company/knubisoft\n" +
+                    "\n" +
                     "Мы отлично поработали) More?\uD83D\uDC47:\n" +
                     "\n" +
-                    "/whoWeAre? \uD83E\uDDD0\n" +
+                    "\uD83E\uDD14 /whoWeAre?\n" +
                     "\n" +
-                    "/whereWeAre? \uD83D\uDCE1\n" +
+                    "\uD83D\uDCE1 /whereWeAre?\n" +
                     "\n" +
-                    "/howWeTeachAndLearn? \uD83D\uDC68\u200D\uD83C\uDF93\n" +
+                    "\uD83D\uDC68\u200D\uD83C\uDF93 /howWeTeachAndLearn?\n" +
                     "\n" +
-                    "/howWeHaveFun? \uD83E\uDD73";
-        else {
-            response = "Catch our links\uD83D\uDC47:\n" +
+                    "\uD83E\uDD73 /howWeHaveFun?";
+            default -> "Catch our links\uD83D\uDC47:\n" +
                     "\n" +
-                    "\uD83C\uDF10 knubisoft.com/\n" +
+                    "\uD83D\uDC68\u200D\uD83D\uDCBB knubisoft.com/\n" +
                     "\n" +
-                    "\uD83C\uDF10 instagram.com/knubisoft/\n" +
-                    "\n" +
-                    "\uD83C\uDF10 linkedin.com/company/knubisoft/mycompany/\n" +
+                    "\uD83D\uDC69\u200D\uD83D\uDCBB linkedin.com/company/knubisoft/\n" +
                     "\n" +
                     "Будем рады видеть тебя снова\uD83E\uDD73";
-        }
+        };
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
         return response;
     }
 }
